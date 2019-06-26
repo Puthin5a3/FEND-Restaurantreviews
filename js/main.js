5e4ef8e228acd4bf20c5f4ef884c615774481c13
Puthin5a3 <51588092+Puthin5a3@users.noreply.github.com>
2019-06-26 10:43:33 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -160,11 +160,11 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
-  image.src = "./"+DBHelper.imageUrlForRestaurant(restaurant);
+  image.src = DBHelper.imageUrlForRestaurant(restaurant);
   image.alt=restaurant.name;
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   name.setAttribute("tabindex","0")
   li.append(name);
